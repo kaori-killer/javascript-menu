98f3d882a64615638279af563e04a9b35805709d
kaori-killer <sksbthwjd@daum.net>
2023-03-18 23:21:25 +0900

    feat: 코치가 못 먹는 메뉴를 ,로 구분해서 입력 받는다, 못 먹는 메뉴가 없다면 빈 값을 입력 받는다

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { readLine, print, close } = MissionUtils.Console;
+
+// ### 메뉴 입력 & 못 먹는 메뉴 입력
+
+// - [x] 코치가 못 먹는 메뉴를 **, 로 구분해서** 입력 받는다.
+// - [x] 코치가 못 먹는 메뉴가 없다면 빈 값을 입력 받는다.
+// - [ ] 코치가 못 먹는 메뉴가 0개 미만이거나 2개를 초과한다면 `코치가 못 먹는 메뉴는 최소 0개 이상 최대 2개 이하로 입력해야 합니다`를 출력한다.
+
 const SAMPLE = {
 	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
 	한식: '김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음',
@@ -8,7 +17,15 @@ const SAMPLE = {
 };
 
 class App {
-  play() {}
+	play() {
+		readLine("못 먹는 메뉴를 입력해 주세요.", (foods) => {
+			const foodList = foods.split(",");
+			close();
+		});
+	}	
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
