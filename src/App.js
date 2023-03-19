2a4cbf5af91c0bb9f4d88d9a278591c7ed8b514d
kaori-killer <sksbthwjd@daum.net>
2023-03-19 12:52:19 +0900

    feat: MissionUtils 라이브러리의 Random.pickNumberInRange() 를 이용해 카테코리를 무작위로 선택한다

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { readLine, print, close } = MissionUtils.Console;
+const { pickNumberInRange } = MissionUtils.Random;
 
-// ### 메뉴 입력 & 못 먹는 메뉴 입력
 
-// - [x] 코치가 못 먹는 메뉴를 **, 로 구분해서** 입력 받는다.
-// - [x] 코치가 못 먹는 메뉴가 없다면 빈 값을 입력 받는다.
-// - [x] 코치가 못 먹는 메뉴가 0개 미만이거나 2개를 초과한다면 `코치가 못 먹는 메뉴는 최소 0개 이상 최대 2개 이하로 입력해야 합니다`를 출력한다.
+// ### 메뉴 추천
+
+// - [x] **MissionUtils 라이브러리의 Random.pickNumberInRange()** 를 이용해 카테코리를 무작위로 선택한다.
+// - [ ] 코치가 못 먹는 메뉴가 포함된 카테고리인 경우 다시 카테고리를 선택한다.
+// - [ ] 2번 이상 추천한 카테고리인 경우 다시 카테고리를 선택한다.
+// - [ ] **MissionUtils 라이브러리의 Random.shuffle()** 이용해 첫 번째 값으로 메뉴를 선택한다.
+// - [ ] 이미 추천한 메뉴라면 다시 섞은 후 첫 번째 값으로 메뉴를 사용한다.
+// - [ ] 위 과정을 4번 더 반복한다.
 
 const SAMPLE = {
 	일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
@@ -16,28 +21,51 @@ const SAMPLE = {
 	양식: '라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니',
 };
 
-// 입출력
+const Category = {
+	1: "일식",
+	2: "한식",
+	3: "중식",
+	4: "아시안",
+	5: "양식",
+}
+
+// 입력
 const InputView = {
 	readFoodList() {
 		readLine("못 먹는 메뉴를 입력해 주세요.", (foods) => {
 			const foodList = foods.split(","); 
-			if(!Validation.IsValidfoodLength(foodList)) this.readFoodList();
-
+			if(Validation.overfoodLength(foodList)) this.readFoodList();
+			app.selectCategory();
 		});
 	}	
 }
 
+// 출력
+const OutputView = {
+	printRecommendMenu(){
+	}
+}
+
 // 예외 처리
 const Validation = {
-	IsValidfoodLength(foodList) {
-		return (foodList.length >= 0 && foodList.length <= 2)
-	}	
+	overfoodLength(foodList) {
+		return (foodList.length > 2);
+	},
+
 }
 
 // 메인
 class App {
+	constructor(){
+		this.coachCategory = [];
+	}
+
 	play() {
 		InputView.readFoodList();
+	}
+
+	selectCategory() {
+		this.coachCategory.push(Category[pickNumberInRange(1, 5)]);
 	}
 }
 
